627188c6a00b71a8ede9be1814d0f877c8a68cb3
ishaharode <ishaharode14@gmail.com>
2023-08-25 15:21:09 -0700

    first occurence of String

---
+++ src/main/java/com/Utils/Main.java
@@ -0,0 +1,12 @@
+package com.Utils;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        int k = FirstOccurrenceOfString.occurrence("sadbutsad","sad");
+        System.out.println("k = " + k);
+
+    }
+}
